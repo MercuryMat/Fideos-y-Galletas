710e45c2c705952b5d04070c512e11d9dd28ab5a
Mateo Amézquita <mateo@qalaub.com>
2023-11-15 17:46:56 -0500

    1.0

--- resources/views/grafica.blade.php
+++ resources/views/grafica.blade.php
@@ -1,21 +1,24 @@
 @extends('layout')
 
 @section('content')
-    <div class="container" style="height: 120vh">
-        <h2>GALLETAS</h2>
-        <div class="graphic-cookies">
-            <canvas id="container-cookies"></canvas>
-        </div>
-
+<div class="container graphic" style="height: 120vh">
+<div class="grapic-container">
         <h2>FIDEOS</h2>
         <div class="graphic-fideos">
             <canvas id="container-fideos"></canvas>
         </div>
     </div>
+    <div class="grapic-container">
+        <h2>GALLETAS</h2>
+        <div class="graphic-cookies">
+            <canvas id="container-cookies"></canvas>
+        </div>
+    </div>    
+</div>
 @endsection
 
 @section('scripts')
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="{!! asset('js/cookies.js') !!}"></script>
-    <script></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="{!! asset('js/cookies.js') !!}"></script>
+<script></script>
 @endsection
